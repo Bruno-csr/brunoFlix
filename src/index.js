6a4b853c8a956a9461d6583a5291bfa7d045b997
Bruno Almeida <bruno.almeida.bca18@gmail.com>
2020-07-28 22:47:21 -0300

    Commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
 
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria'
+
+
+//const de erro - desafio de colocar um jogo na página de erro
+const paginaErro = () => <div>Página 404</div>
+
+//exact - exatamente o path 
+//ordem importa 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path='/cadastro/video' component={CadastroVideo} />
+      <Route path='/cadastro/Categoria' component={CadastroCategoria} />
+      <Route path='/' component={Home} exact/> 
+      <Route component={paginaErro} /> 
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
